f4197600fa09a4c48f77bf1a1dff03aa83888463
OlegDjur <olegdjur@gmail.com>
2023-04-23 16:59:05 +0600

    Created function GetUser in repository

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.20
 
 require github.com/labstack/echo v3.3.10+incompatible
 
+require github.com/pkg/errors v0.8.1 // indirect
+
 require (
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.0 // indirect
@@ -12,6 +14,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v4 v4.18.1 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -3,9 +3,13 @@ package repository
 import (
 	"context"
 	"echo-template/pkg/domain"
+	"errors"
 
+	"github.com/jackc/pgx"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
+
+var ErrUserNotFound = errors.New("error: User not found")
 
 type Repository interface {
 	CreateUser(ctx context.Context, request domain.UserRequest) (domain.User, error)
@@ -25,6 +29,19 @@ func (r *repository) CreateUser(ctx context.Context, request domain.UserRequest)
 
 	query := `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`
 	if err := r.db.QueryRow(ctx, query, request.Name, request.Email, request.Password).Scan(&response); err != nil {
+		return domain.User{}, err
+	}
+
+	return response, nil
+}
+
+func (r *repository) GetUser(ctx context.Context, id int) (domain.User, error) {
+	var response domain.User
+
+	if err := r.db.QueryRow(ctx, "SELECT * FROM users WHERE id = $1", id).Scan(&response); err != nil {
+		if err == pgx.ErrNoRows {
+			return domain.User{}, ErrUserNotFound
+		}
 		return domain.User{}, err
 	}
 
